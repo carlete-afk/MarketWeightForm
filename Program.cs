94f43e433c5f5ff438432ae1b36ffc4ed3e3e4d2
carlete-afk <carlos.belloet12de1@gmail.com>
2024-10-09 15:22:34 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Text;
+using System.Configuration;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+public class Conexion
+{
+    MySqlConnection connection = new MySqlConnection();
+
+    static string servidor = "localhost";
+    static string bd = "bdmysql";
+    static string usuario = "root";
+    static string password = "root";
+    static string puerto = "3307";
+
+    string connectionString = $"server={servidor}; port={puerto}; user id={usuario}; password={password}; database={bd};";
+
+
+    public MySqlConnection Conectar ()
+    {
+        try
+        {
+            connection.ConnectionString = connectionString;
+            connection.Open();
+            MessageBox.Show("Lograste meterte en la BD");
+        }
+
+        catch (MySqlException e )
+        {
+            MessageBox.Show("No se puede conctar a la BD, error:" + e.ToString());
+        }
+
+        return connection;
+    }
+}
